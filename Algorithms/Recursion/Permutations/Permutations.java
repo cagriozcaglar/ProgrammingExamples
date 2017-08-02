967ee8f0c9102830f1be6d4cdf787f624ab2ee85
Cagri Ozcaglar <cagri@Cagris-MacBook-Pro.local>
2017-08-02 08:22:21 -0700

    Added changed to files that are modified / deleted. Updated commit commands.

--- Algorithms/Recursion/Permutations/Permutations.java
+++ Algorithms/Recursion/Permutations/Permutations.java
@@ -1,7 +1,77 @@
-package Recursion.Permutations;
-
 /**
- * Created by cagri on 7/23/17.
+ * Return all distinct permutations of the given characters.
+ * For example, generatePermutations("abc") returns {"abc", "acb", "bac", "bca", "cab", "cba"}
+ * Two versions: 1) All permutations, 2) All distinct permutations.
  */
-public class Permutations {
+
+import java.util.*;
+
+public class Permutations{
+
+    // Main runner of permutation generator
+    public static ArrayList<String> generatePermutations(String s){
+        ArrayList<String> permutations = new ArrayList<String>();
+        if(s.length() == 0)
+            return permutations;
+        boolean[] visited = new boolean[s.length()];
+        generate(s, 0, visited, "", permutations);
+        return permutations;
+    }
+
+    // Recursive permutation generator
+    public static void generate(String s, int pos, boolean[] visited, String curr, List<String> perms){
+        // Base case: Get a complete permutation
+        if(pos == s.length()){
+            perms.add(curr);
+            return;
+        }
+        // Recursive step
+        for(int i = 0; i < s.length(); i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                generate(s, pos+1, visited, curr + s.charAt(i), perms);
+                visited[i] = false;
+            }
+        }
+    }
+
+    // Main runner of permutation generator: Distinct permutations
+    public static HashSet<String> generatePermutationsSet(String s){
+        HashSet<String> permutations = new HashSet<String>();
+        if(s.length() == 0)
+            return permutations;
+        boolean[] visited = new boolean[s.length()];
+        generateSet(s, 0, visited, "", permutations);
+        return permutations;
+    }
+
+    // Recursive permutation generator: Distinct permutations
+    public static void generateSet(String s, int pos, boolean[] visited, String curr, HashSet<String> perms){
+        // Base case: Get a complete permutation
+        if(pos == s.length()){
+            perms.add(curr);
+            return;
+        }
+        // Recursive step
+        for(int i = 0; i < s.length(); i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                generateSet(s, pos+1, visited, curr + s.charAt(i), perms);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        // Test 1: "abc"
+        ArrayList<String> permutationList1 = Permutations.generatePermutations("abc");
+        HashSet<String> permutationSet1 = Permutations.generatePermutationsSet("abc");
+        System.out.println(permutationList1);
+        System.out.println(permutationSet1);
+        // Test 2: "abba"
+        ArrayList<String> permutationList2 = Permutations.generatePermutations("abba");
+        HashSet<String> permutationSet2 = Permutations.generatePermutationsSet("abba");
+        System.out.println(permutationList2);
+        System.out.println(permutationSet2);
+    }
 }
--- commit.sh
+++ commit.sh
@@ -1,7 +1,4 @@
 # git config --system core.editor emacs
-git add --all .
-git commit -m "$1"
-randomText="origin`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 6`"
-git remote add ${randomText} https://github.com/cagriozcaglar/ProgrammingExamples.git
-git remote -v
-git push ${randomText} master
+# git add --all .
+# git commit -m "$1"
+# git push
